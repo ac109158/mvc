bc022a553ed3ccf488c65bbd427a83a8255d189a
Andy Cook <ac109158@plusonecompany.info>
2013-07-03 01:26:51 -0600

    Small Changes

---
+++ application/models/baseModel.php
@@ -0,0 +1,10 @@
+<?php
+class BaseModel extends Model
+	{
+    public function __construct()
+    {
+        parent::__construct();
+
+    }	
+	
+	}
---
+++ application/models/errorModel.php
@@ -0,0 +1,11 @@
+<?php
+class ErrorModel extends Model
+	{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->db_errors = '';
+        $this->form_errors = '';
+    }	
+	
+	}
