00c7ade75364d3022484045c33efef17f14974cb
Andy Cook <ac109158@plusonecompany.info>
2013-07-24 18:55:06 -0600

    UPDATE: GROUP CHAT IS WORKING BUT CSS IS A MESS

---
+++ chat-widget/chat.php
@@ -0,0 +1,51 @@
+ 
+<!--     <link rel="stylesheet/less" type="text/css" href="chat-widget/css/twitter-bootstrap/lib/bootstrap.less"> -->
+    <script src="chat-widget/css/less/less-1.1.5.min.js"></script>
+    
+    <link href="chat-widget/css/styles.css" rel="stylesheet" />
+    <link href="chat-widget/css/pusher-chat-widget.css" rel="stylesheet" />
+    
+    <!--[if lt IE 9]>
+    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    
+    <script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+    <script src="http://js.pusher.com/2.1/pusher.min.js"></script>
+    <script src="chat-widget/js/PusherChatWidget.js"></script>    
+    <script>
+    
+      $(function() {
+/*       	  var user = "<?php echo $_SESSION['name_of_user']?>"; */
+		var user = "andy";	
+		  var pusher = new Pusher('71d3c4ea25f712272ad3');    
+         var chatWidget = new PusherChatWidget(pusher, {
+          appendTo: '#pusher_chat_widget'
+        });
+           
+      });
+	
+    </script>
+
+          <div class="span5" id='pusher_chat_widget'>
+          </div>
+          
+        </div>
+        
+      </section>
+
+      <footer>
+        <p></p>
+      </footer>
+
+    </div> <!-- /container -->
+    
+    <script>
+$(document).keypress(function(event){
+
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+        $('#pusher-chat-widget-send-btn').click();   
+    }
+
+});
+   </script>
---
+++ chat-widget/config_example.php
@@ -0,0 +1,7 @@
+<?php
+error_reporting(E_ALL);
+
+define('APP_KEY', '71d3c4ea25f712272ad3');
+define('APP_SECRET', 'c42cc7e374a646effacb');
+define('APP_ID', '49579');
+?>
